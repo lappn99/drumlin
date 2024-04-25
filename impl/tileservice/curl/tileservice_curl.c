e9798f49c09ac5a4cd60a895f35c557dfcafafe8
lappn99 <nathanglapp@gmail.com>
2024-04-25 12:30:08 -0400

    Get rid of stb_image_write

--- impl/tileservice/curl/tileservice_curl.c
+++ impl/tileservice/curl/tileservice_curl.c
@@ -11,9 +11,6 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
-
 
 #include <drumlin/app.h>
 #include <drumlin/map.h>
